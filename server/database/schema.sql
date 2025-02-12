4fdcbc5b752b64a8586b8d6710dc9daaf224704c
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-02-12 14:34:19 +0100

    🗃️: name est maintenant unique et ajout de 2 éléments de test

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,7 +1,7 @@
 create table monsters (
   id int unsigned primary key auto_increment not null,
   picture_name varchar(50) not null,
-  name varchar(50) not null,
+  name varchar(50) not null UNIQUE,
   family varchar(50) not null,
   health int unsigned not null,
   action_points int unsigned not null,
@@ -17,4 +17,6 @@ create table monsters (
 insert into monsters(name, picture_name, family, health, action_points, movement_points, res_neu, res_fo, res_ine, res_cha, res_age)
 values
   ("Bouftou", "bouftou.png", "Bouftous", 170, 5, 4, 1, 11, -14, -9, -16),
-  ("Piou Rouge", "piou_rouge.png", "Pious", 49, 3, 3, 0, 0, 25, 0, 0);
+  ("Piou Rouge", "piou_rouge.png", "Pious", 49, 3, 3, 0, 0, 25, 0, 0),
+  ("Bouftou2", "bouftou.png", "Bouftous", 170, 5, 4, 1, 11, -14, -9, -16),
+  ("Bouftou3", "bouftou.png", "Bouftous", 170, 5, 4, 1, 11, -14, -9, -16);
