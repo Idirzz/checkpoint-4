1246f9d7ceac8625e818cfc64b8734e43d057d03
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-02-12 10:01:24 +0100

    🚧: Création de la structure des pages

---
+++ client/src/pages/CharacterPage/CharacterPage.tsx
@@ -0,0 +1,10 @@
+import "./CharacterPage.css";
+
+function CharacterPage() {
+  return (
+    <div className="character-page-main-container">
+      Ceci est la page personnage
+    </div>
+  );
+}
+export default CharacterPage;
---
+++ client/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,6 @@
+import "./HomePage.css";
+
+function HomePage() {
+  return <div className="homepage-main-container">Ceci est la homepage</div>;
+}
+export default HomePage;
