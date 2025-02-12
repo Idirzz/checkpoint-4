2bff685f52787bef541e49564edee522d0c10cd0
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-02-12 12:23:45 +0100

    🏷️: Create server/client Monster type

---
+++ server/src/types/Monsters.d.ts
@@ -0,0 +1,14 @@
+export interface Monster {
+  id: number;
+  picture_name: string;
+  name: string;
+  family: string;
+  health: number;
+  action_points: number;
+  movement_points: number;
+  res_neu: number;
+  res_fo: number;
+  res_ine: number;
+  res_cha: number;
+  res_age: number;
+}
