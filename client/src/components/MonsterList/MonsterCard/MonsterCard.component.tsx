48fc9d68af01e244319c55e325eadf2af2db0702
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-02-12 14:35:57 +0100

    💄: Style et lien des cartes monstres

--- client/src/components/MonsterList/MonsterCard/MonsterCard.component.tsx
+++ client/src/components/MonsterList/MonsterCard/MonsterCard.component.tsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 import type { Monster } from "../../../types/Monster";
 import "./MonsterCard.component.css";
 
 function MonsterCard({ data }: { data: Monster }) {
   return (
-    <li className="monstercard-container pointer">
-      <img
-        alt={`${data.name}`}
-        src={`${import.meta.env.VITE_API_URL}/images/${data.picture_name}`}
-      />
-      <p>{data.name}</p>
-    </li>
+    <Link to={`/monstre/${data.name}`}>
+      <li className="monstercard-container">
+        <img
+          alt={`${data.name}`}
+          src={`${import.meta.env.VITE_API_URL}/images/${data.picture_name}`}
+        />
+        <p>{data.name}</p>
+      </li>
+    </Link>
   );
 }
 export default MonsterCard;
