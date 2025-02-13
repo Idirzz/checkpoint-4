be354a5851ab606d2b16f91815fe39a978d48e69
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-02-13 11:39:27 +0100

    🧑‍💻: Formulaire modularisé importable

--- client/src/pages/CreationPage/CreationPage.tsx
+++ client/src/pages/CreationPage/CreationPage.tsx
@@ -1,5 +1,6 @@
 import type { FormEvent } from "react";
 import "./CreationPage.css";
+import MonsterForm from "./MonsterForm";
 
 function CreationPage() {
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
@@ -12,55 +13,7 @@ function CreationPage() {
   }
   return (
     <main className="creation-page-main-container">
-      <form onSubmit={handleSubmit} className="creation-page-content-container">
-        <h1>Créer un nouveau monstre</h1>
-        <label>
-          Image :
-          <input name="picture" type="file" />
-        </label>
-        <label>
-          Nom :
-          <input name="name" type="text" />
-        </label>
-        <label>
-          Famille :
-          <input name="family" type="text" />
-        </label>
-        <label>
-          Points de vie :
-          <input name="health" type="text" />
-        </label>
-        <label>
-          Points de mouvement :
-          <input name="movementPoints" type="text" />
-        </label>
-        <label>
-          Points d'action :
-          <input name="actionPoints" type="text" />
-        </label>
-        <h3>Résistances :</h3>
-        <label>
-          Neutre :
-          <input name="resNeu" type="text" />
-        </label>
-        <label>
-          Force :
-          <input name="resFo" type="text" />
-        </label>
-        <label>
-          Intelligence :
-          <input name="resIne" type="text" />
-        </label>
-        <label>
-          Chance :
-          <input name="resCha" type="text" />
-        </label>
-        <label>
-          Agilité :
-          <input name="resAge" type="text" />
-        </label>
-        <input type="submit" />
-      </form>
+      <MonsterForm onSubmit={handleSubmit} />
     </main>
   );
 }
