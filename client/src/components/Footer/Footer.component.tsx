b4833192f4414e8a4960a23d56d482625f7effb9
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-02-13 11:23:45 +0100

    🩹: Fix pas de balise p autour de texte

--- client/src/components/Footer/Footer.component.tsx
+++ client/src/components/Footer/Footer.component.tsx
@@ -1,6 +1,10 @@
 import "./Footer.component.css";
 
 function Footer() {
-  return <footer className="footer-main-container">Ceci est le footer</footer>;
+  return (
+    <footer className="footer-main-container">
+      <p>Ceci est le footer</p>
+    </footer>
+  );
 }
 export default Footer;
